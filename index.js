e4adabf5c63ed51fe6f9e53ca649feb20e730c52
Maxwell Dubin <maxdhs@gmail.com>
2023-08-29 12:19:59 -0400

    ...

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const prisma = require("./prisma");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.send({ success: true, users });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+});
+
+app.put("/users/:userId", async (req, res) => {
+  try {
+    const { count } = req.body;
+    const user = await prisma.user.update({
+      where: { id: req.params.userId },
+      data: { count },
+    });
+    res.send({ success: true, user });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const { name } = req.body;
+    const user = await prisma.user.create({
+      data: { name },
+    });
+    res.send({ success: true, user });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+});
+
+app.delete("/users/:userId", async (req, res) => {
+  try {
+    const user = await prisma.user.delete({ where: { id: req.params.userId } });
+    res.send({ success: true, user });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+});
+
+app.listen(3000, () => console.log("server is up"));
